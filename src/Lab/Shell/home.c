bedd7631f712f5f9834be44c68987c80721e3324
mehulagarwal <mehul96mail@gmail.com>
2016-08-27 19:31:32 +0530

    Commit on 27/8

--- src/Lab/Shell/home.c
+++ src/Lab/Shell/home.c
@@ -2,15 +2,84 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <string.h>
+#include <malloc.h>
+
+
+int val;
+char *arrWords[100];
+
+
+void display()
+{
+	int i;
+	for(i=0;i<=val;i++)
+	{
+		if(arrWords[i] != NULL)
+		{
+			printf("%s\t",arrWords[i]);
+		}
+	}
+	printf("\n");
+}
+
+
+void parse(char *str)
+{
+
+	int length = strlen(str);
+	arrWords[val] = (char *)malloc(sizeof(char)* length+1);
+	strcpy(arrWords[val],str);
+	val++;
+
+}
+
+void addToList(char *sentence)
+{
+	int length = strlen(sentence);
+	char *word = (char *)calloc(1, sizeof(char));
+	int wordI=0;
+	char temp;
+	int i;
+	for(i=0;i<length;i++)
+	{
+		temp = sentence[i];
+		if(temp == ' ' || temp == '\n' ||temp == '\t' || temp == '\0')
+		{
+			parse(word);
+			word = (char *)(calloc(1,sizeof(char)));
+			wordI = 0;
+			continue;
+		}
+		else
+		{
+			word = realloc(word,sizeof(char) * (wordI+1));
+			word[wordI] = temp;
+			wordI++;
+		
+		}
+	}
+	parse(word);
+	word = (char *)(calloc(1,sizeof(char)));
+	wordI = 0;
+}
 
 int main()
 {
 	
 	struct passwd *pw = getpwuid(getuid());
-
+	val = 0;
 	const char *homedir = pw->pw_dir;
 	printf("%s \n", homedir);
-	char str[20];
-	gets(str);
-	printf("%s \n", str);
+	char str[30];
+	//char *arrWords[100];
+	int i;	
+	for(i=0;i<3;i++)
+	{
+		gets(str);
+		addToList(str);
+	}
+	printf("Printing the final array\n");
+	display();
+
 }
--- src/Lab/Shell/try2.c
+++ src/Lab/Shell/try2.c
@@ -3,30 +3,100 @@
 #include <string.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <stdlib.h>
+#include <malloc.h>
 
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+int val;
+char **arrWords;
+int commandCount;
+int currentIndex;
+
+void display()
+{
+	int i;
+	for(i=0;i<=val;i++)
+	{
+		if(arrWords[i] != NULL)
+		{
+			printf("%s\t",arrWords[i]);
+		}
+	}
+	printf("\n");
+}
+
+
+void parse(char *str)
+{
+
+	int length = strlen(str);
+	arrWords[val] = (char *)malloc(sizeof(char)* length+1);
+	strcpy(arrWords[val],str);
+	val++;
+	commandCount++;
+	arrWords[val] = '\0';
+
+}
+
+void addToList(char *sentence)
+{
+	int length = strlen(sentence);
+	char *word = (char *)calloc(1, sizeof(char));
+	int wordI=0;
+	char temp;
+	int i;
+	for(i=0;i<length;i++)
+	{
+		temp = sentence[i];
+		if(temp == ' ' || temp == '\n' ||temp == '\t' || temp == '\0')
+		{
+			parse(word);
+			word = (char *)(calloc(1,sizeof(char)));
+			wordI = 0;
+			continue;
+		}
+		else
+		{
+			word = realloc(word,sizeof(char) * (wordI+1));
+			word[wordI] = temp;
+			wordI++;
+		
+		}
+	}
+	parse(word);
+	word = (char *)(calloc(1,sizeof(char)));
+	wordI = 0;
+}
+
 int main()
 {
+	commandCount = 0;
+	currentIndex = 0;
 	struct passwd *pw = getpwuid(getuid());
 	const char *homedir = pw->pw_dir;
 	int pid;
 	char com[20];
 	char path[200];
-	char *arg[100];
+	val = 0;
+	//char *arg[100];
 	
 	//char path2[205];
 	while(1)
 	{
-		
-		
+	val = 0;
+	arrWords = (char**)realloc(arrWords,(100*sizeof(char)));
+	currentIndex += commandCount;
+
 	getcwd(path,sizeof(path));
 	printf(ANSI_COLOR_GREEN  "MyShell: $ "  ANSI_COLOR_RESET, path);
 	//printf(path2);		
 	gets(com);
-	parse(com, arg);
-	char *arg[] = {"", NULL};
-	arg[0] = com;
+	//parse(com, arrWords);
+	addToList(com);
+	//char *arg[] = {"", NULL};
+	//arg[0] = com;
 	pid = fork();
 	if(pid == 0)
 	{
@@ -35,7 +105,7 @@ int main()
 		printf("Goodbye\n");
 		return;
 	}
-	execvp(arg[0], arg);
+	execvp(arrWords[0], arrWords);
 	exit(0);
 	}
 	else
@@ -49,18 +119,4 @@ int main()
 
 }
 
-void parse(char *com, char **arg)
-{
-	char temp;
-	temp = com[0];
-	int count = 0;
-	while(com != '\0' && count < 20)
-	{
-		if(temp == ' ' || temp = '\t' || temp == '\n')
-		{
-			temp = '\0';
-		}
-		count++;
-	}
-}
 
